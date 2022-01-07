3353a8b60bc220e3b1dd3d5d431c4bdb0587afa5
Felipe Sakuma <fycorsak@gmail.com>
2022-01-07 18:18:16 -0300

    services file working

--- src/components/pages/SignInPage.js
+++ src/components/pages/SignInPage.js
@@ -1,9 +1,9 @@
 import { useContext, useState } from "react"
 import { Container, Form, Input, Button, StyledLink } from '../page components/styledComponents';
-import axios from "axios";
 import Loading from "../page components/Loader";
 import Logo from "../page components/Logo";
 import Context from "../contexts/Context";
+import { handleSignInSubmit } from "../services/services";
 
 export default function SignInPage() {
   const [formValues, setFormValues] = useState({
@@ -17,23 +17,11 @@ export default function SignInPage() {
     setFormValues({ ...formValues, [e.target.name]: e.target.value })
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setIsLoading(true);
-    axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login', formValues)
-      .then((response) => {
-        console.log(response);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.log(error.response);
-        setIsLoading(false);
-      })
-  }
+
   return (
     <Container>
       <Logo />
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={(e) => handleSignInSubmit(e, 'auth/login', formValues, setIsLoading)}>
         <Input
           type="email"
           placeholder="email"
---
+++ src/components/services/services.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const BASE_URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/'
+
+export function handleSignInSubmit(e, url, toSend, setIsLoading) {
+
+  e.preventDefault();
+  setIsLoading(true);
+  axios.post(`${BASE_URL}${url}`, toSend)
+    .then((response) => {
+      console.log(response);
+      setIsLoading(false);
+    })
+    .catch((error) => {
+      console.log(error.response);
+      setIsLoading(false);
+    })
+}
+
+export function handleSignUpSubmit(e, url, toSend, setIsLoading) {
+  return new Promise((resolve, reject) => {
+    e.preventDefault();
+    setIsLoading(true);
+    axios.post(`${BASE_URL}${url}`, toSend)
+      .then(() => {
+        setIsLoading(false);
+        toast.success(`Usuário criado com sucesso!
+        Você será redirecionado para a página de login`, {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        resolve()
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toast.error(error.response.data.message === 'Campo "body" inválido!' ? 'Email inválido' : error.response.data.message, {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        reject();
+      })
+  })
+}
+const exportedObject = { handleSignInSubmit, handleSignUpSubmit }
+export default exportedObject;
